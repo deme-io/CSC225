0e059081bc35a2710ffd1bb53ceb723241e2e1f2
Demetrius Henry <deme@deme.io>
2016-08-26 19:43:38 -0700

    Problem Set 1

---
+++ Problem Set 1/House.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class House here.
+ * 
+ * Demetrius Henry 
+ * CSC225
+ * Exercise #1
+ */
+public class House
+{
+    public static void main(String[] args)
+    {
+        System.out.println("-----------");
+        System.out.println(" |   [|] |");
+        System.out.println(" |       |");
+        System.out.println(" |   [|] |");
+        System.out.println(" |    _  |");
+        System.out.println(" |   | | |       *");
+        System.out.println(" |   | | |      *|*");
+        System.out.println(" |___|_|_|_______|");
+        
+    }
+}
