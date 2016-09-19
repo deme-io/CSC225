c4e30fcb491cf935336de61ab3f205954ce55e33
Demetrius Henry <demetrius.henry@me.com>
2016-09-18 17:54:05 -0700

    Problem Set 4 - Number 7

--- Problem Set 4/StringSort7.java
+++ Problem Set 4/StringSort7.java
@@ -16,16 +16,38 @@ public class StringSort7
         String first;
         String second;
         String third;
+        String outcome;
         
         System.out.println("");
         System.out.print("Enter the first character's name: ");
         first = input.next().trim();
         
-        System.out.println("");
         System.out.print("Enter the second character's name: ");
         second = input.next().trim();
         
-        System.out.println("");
         System.out.print("Enter the third character's name: ");
         third = input.next().trim();
+        
+        int firstSecond = first.compareTo(second);
+        int firstThird = first.compareTo(third);
+        int secondThird = second.compareTo(third);
+        
+        if (firstSecond < 0 && firstThird < 0)
+            if (secondThird < 0)
+                outcome = first + ", " + second + ", " + third;
+            else
+                outcome = first + ", " + third + ", " + second;
+        else if (secondThird < 0 && firstSecond > 0)
+            if (firstThird < 0)
+                outcome = second + ", " + first + ", " + second;
+            else
+                outcome = second + ", " + third + ", " + first;
+        else
+            if (firstSecond < 0)
+                outcome = third + ", " + first + ", " + second;
+            else
+                outcome = third + ", " + second + ", " + first;
+                
+        System.out.print(outcome);
+    }    
 }
